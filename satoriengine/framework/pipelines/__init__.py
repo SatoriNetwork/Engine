a4843c34a0e0da3a482a72526e68854ef27a6283
krizhnaa <krizhnaprem@gmail.com>
2024-11-24 13:18:01 +0530

    filename change

--- satoriengine/framework/pipelines/__init__.py
+++ satoriengine/framework/pipelines/__init__.py
@@ -1,4 +1,4 @@
 from satoriengine.framework.pipelines.interface import PipelineInterface, TrainingResult
 from satoriengine.framework.pipelines.sk import SKPipeline
 from satoriengine.framework.pipelines.starter import StarterPipeline
-from satoriengine.framework.pipelines.simple_xgb import XgbPipeline
+from Engine.satoriengine.framework.pipelines.xgb import XgbPipeline
