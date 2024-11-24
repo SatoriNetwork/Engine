3046976b85cbbc86d6c3b38eda460e1fe70ed862
krizhnaa <krizhnaprem@gmail.com>
2024-11-24 13:30:19 +0530

    fix

--- satoriengine/framework/pipelines/__init__.py
+++ satoriengine/framework/pipelines/__init__.py
@@ -1,4 +1,4 @@
 from satoriengine.framework.pipelines.interface import PipelineInterface, TrainingResult
 from satoriengine.framework.pipelines.sk import SKPipeline
 from satoriengine.framework.pipelines.starter import StarterPipeline
-from Engine.satoriengine.framework.pipelines.xgb import XgbPipeline
+from satoriengine.framework.pipelines.xgb import XgbPipeline
